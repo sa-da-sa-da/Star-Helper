5485f70b3d4a9e2670c5ff1359cd3e09875767dd
sa-da-sa-da <56340246+sa-da-sa-da@users.noreply.github.com>
2021-10-07 01:06:49 +0800

    found

---
+++ miniprogram/pages/found/index/index.js
@@ -0,0 +1,13 @@
+// pages/found/index/index.js
+Page({
+  data: {
+    TabCur: 0,
+    scrollLeft:0
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id-1)*60
+    })
+  }
+})
---
+++ miniprogram/pages/found/search/index.js
@@ -0,0 +1,135 @@
+// pages/work/search/index.js
+
+import {config} from '../../../utils/config.js'
+const db = wx.cloud.database({
+  env: config.EnvId
+})
+const _ = db.command
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    searchContent: "",
+    lists: [],
+    showTips: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: "搜索"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  /**
+   * 获取input中的搜索内容
+   */
+  searchInput: function (e) {
+    this.setData({
+      searchContent: e.detail.value
+    })
+  },
+  /**
+   * 搜索
+   */
+  submitSearch: function (e) {
+    let that = this;
+    wx.showLoading({
+      title: '搜索中...',
+    })
+    try {
+      let where = {}
+      where.mec = db.RegExp({
+        regexp: '.*' + that.data.searchContent,
+        options: 'i',
+      })
+      // 正则表达式搜索
+      db.collection('press')
+        .where(where)
+        .get({
+          success: res => {
+            if (res.data.length > 0) {
+              that.setData({
+                lists: res.data,
+              })
+            } else {
+              that.setData({
+                lists:[],
+              })
+              Dialog.alert({
+                title: '温馨提示',
+                message: '未搜索到相关校招 请尝试其他关键词搜索',
+                theme: 'round-button',
+              }).then(() => {
+                // on close
+              });
+            }
+            that.setData({
+              showTips: false,
+            })
+          },
+          fail: err => {
+            console.log(err)
+          }
+        })
+    } catch (err) {
+      console.info(err)
+    } finally {
+      wx.hideLoading()
+    }
+
+  }
+})
